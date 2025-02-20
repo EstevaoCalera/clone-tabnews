be9a784184ec81ef6e951ad955db02b297e8327c
Estêvão Calera <estevaoscalera@gmail.com>
2025-02-20 18:45:50 -0300

    Add format and increase readability

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Hi! I'm a message that just surfed the internet to drop you a virtual kiss 😘 and bring a smile 😄 in your face in return!</h1>
+  return (
+    <h1>
+      {`Hi! I'm a message that just surfed the internet to drop you a virtual kiss 😘 
+      and bring a smile in your face in return! 😄`}
+    </h1>
+  );
 }
 
 export default Home;
